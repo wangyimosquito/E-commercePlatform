3d85ae2d546643eed171505876a04d65156dc56f
wangyiwen <yiwenw315@gmail.com>
2023-03-04 22:22:13 -0800

    add update and add function

--- Backend/src/main/java/com/yiwen/mapper/BrandMapper.java
+++ Backend/src/main/java/com/yiwen/mapper/BrandMapper.java
@@ -7,11 +7,18 @@ import java.util.List;
 import java.util.Map;
 
 public interface BrandMapper {
+    /*
+    * return all brands
+    * */
     List<Brand> selectAll();
 
-    Brand selectById(int id);
     /*
-    * three ways to get params
+    * return with certain ID
+    * */
+    Brand selectById(int id);
+
+    /*
+    * Dynamic query using three params
     * */
     List<Brand> selectByCondition(@Param("status")int status,
                                   @Param("companyName")String companyName,
@@ -19,4 +26,11 @@ public interface BrandMapper {
 
     List<Brand> selectByCondition(Brand brand);
     List<Brand> selectByCondition(Map map);
+
+    /*
+    * add a new brand to the database
+    * */
+    void add(Brand brand);
+
+    void update(Brand brand);
 }
